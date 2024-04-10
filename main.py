6de9c5fc6a11f3dc041af65c1ce11a0bac5728f5
Mohit Garg <mohit.x.garg@haleon.com>
2024-04-10 17:44:52 +0530

    run updated

--- main.py
+++ main.py
@@ -68,5 +68,6 @@ def validate_token(authorization = Header(None)):
         print('Exception Occurred')
         raise token_exception
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port= 8000, log_level="info")
